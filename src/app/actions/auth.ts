04d21257513b65f27dc386c513776fac82bed160
Anton Kozyr <anton.kozyr.dev@gmail.com>
2025-01-20 18:08:54 +0100

    * Update the Login page components

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -3,23 +3,57 @@
 import { createHash } from "node:crypto";
 import { createSession } from "../lib/session";
 import { redirect } from "next/navigation";
+import { z } from "zod";
+
+export type FormState = {
+  errors?: {
+    email?: string[];
+    password?: string[];
+  };
+  errorMessage?: string;
+} | undefined;
+
+type UserInputData = {
+  email: string;
+  password: string;
+};
+
+export async function login(previousState: FormState, formData: FormData): Promise<FormState> {
+  const validatedFields = LoginFormSchema.safeParse({
+    email: formData.get('email'),
+    password: formData.get('password')
+  });
+
+  if (!validatedFields.success) {
+    return { errors: validatedFields.error.flatten().fieldErrors };
+  }
+
+  const { email, password } = validatedFields.data;
+  if (!isAuthenticated({ email, password })) {
+    return { errorMessage: 'Invalid credentials.' };
+  }
+
+  await createSession(email);
+  redirect('/');
+}
+
+const LoginFormSchema = z.object({
+  email: z.string().email({ message: 'Please enter a valid email.' }).trim(),
+  password: z
+    .string()
+    .min(4, { message: 'Be at least 4 characters long' })
+    .regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
+    .trim()
+});
 
 const defaultUser = Object.freeze({
   email: 'anton.kozyr.dev@gmail.com',
   password: '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08'
 });
 
-export async function login(formData: FormData) {
-  const email = formData.get('email') as string;
-  const password = formData.get('password') as string;
-
-  if (!(email === defaultUser.email && generateHash(password) === defaultUser.password)) {
-    throw Error('forbidden.');
-  }
-
-  await createSession(email);
-
-  redirect('/');
+function isAuthenticated(data: UserInputData) {
+  const { email, password } = data;
+  return email === defaultUser.email && generateHash(password) === defaultUser.password;
 }
 
 function generateHash(input: string) {
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,17 @@
+'use client';
+
+import { useActionState } from 'react';
 import { login } from '../actions/auth';
+import ErrorAlert from '../components/error-alert';
 
 export default function LoginPage() {
+  const [state, formAction, pending] = useActionState(login, undefined);
+
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
         <h2 className="text-2xl font-bold text-center text-gray-800">Login</h2>
-        <form action={login} className="mt-6">
+        <form action={formAction} className="mt-6">
           <div className="mb-4">
             <input
               id="email"
@@ -15,6 +21,7 @@ export default function LoginPage() {
               placeholder="Email"
               required
             />
+            {state?.errors?.email && <p className="text-red-500 text-xs italic">{state.errors.email}</p>}
           </div>
           <div className="mb-6">
             <input
@@ -25,8 +32,20 @@ export default function LoginPage() {
               placeholder="Password"
               required
             />
+            {state?.errors?.password && (
+              <div className="text-red-500 text-xs italic">
+                <p>Password must:</p>
+                <ul>
+                  {state.errors.password.map((error) => (
+                    <li key={error}>- {error}</li>
+                  ))}
+                </ul>
+              </div>
+            )}
           </div>
+          {state?.errorMessage && <ErrorAlert title="Invalid credentials!" reason="The pair of email and password is not correct." />}
           <button
+            disabled={pending}
             type="submit"
             className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
           >
